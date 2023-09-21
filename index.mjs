d133da3ca0dacc54d89e49f77109e4d3de05b68e
Iván Serrano Martin <vancete07@gmail.com>
2023-09-21 23:46:41 +0200

    Now the titles are loaded from a txt list file instead of passing a link as parameter each time

--- index.mjs
+++ index.mjs
@@ -1,76 +1,92 @@
-import fs from "node:fs";
-import * as pt from "puppeteer";
-import { cleanFileName } from "./utils/utils.mjs";
+import fs from 'node:fs'
+import * as pt from 'puppeteer'
+import { timeout, cleanFileName, readGameListFile } from './utils/utils.mjs'
 
-function timeout(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+const gameList = readGameListFile()
+
+async function getGameInfo(game, index) {
+    let gameData = {}
+    const browser = await pt.launch({ headless: 'new' })
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1000, height: 500 })
+
+    try {
+        await page.goto('https://opencritic.com/search')
+
+        /*
+          Busca el juego extraido del game-list en el buscador de opencritic
+        */
+        const searchSelector = 'input[name="searchStr"]'
+        await page.$(searchSelector)
+        await page.type(searchSelector, game.toLowerCase())
+        await page.focus(searchSelector)
+        await page.keyboard.press('Enter')
+        await timeout(2000)
+        const firstElementInSearch = await page.$eval('.results-container > div a', (element) => element.href)
+
+        console.log(`${index + 1}/${gameList.length}`)
+        console.log('Encontrado el juego', game, firstElementInSearch)
+
+        /*
+          Obtiene los datos del juego desde opencritic
+        */
+        await page.goto(firstElementInSearch)
+
+        const jsonElement = await page.$('script[type="application/ld+json"]')
+        const jsonString = await page.evaluate((element) => element.textContent, jsonElement)
+
+        const jsonObject = JSON.parse(jsonString)
+        gameData.data = jsonObject
+        const gameName = jsonObject.name
+
+        console.log('Datos del juego extraidos', gameName)
+
+        /*
+          Busca el link en cdkeys a partir del nombre del juego
+        */
+        await page.goto('https://cdkeys.cheap/')
+
+        const inputSelector = '.search-form__field'
+        await page.$(inputSelector)
+        await page.type(inputSelector, gameName.toLowerCase())
+        await timeout(2000)
+        await page.$('#searchList')
+        const firstElementInSearchList = await page.$eval('#searchList > :first-child a', (element) => element.href)
+
+        console.log('Enlace del juego buscado obtenido de CDKeys', firstElementInSearchList)
+
+        /*
+          Obtiene los links de las tiendas desde cdkeys
+        */
+        await page.goto(firstElementInSearchList)
+
+        const linkElements = await page.$$eval('.offer__btn', (buttons) => {
+            return buttons.map((button) => {
+                return button.href
+            })
+        })
+
+        gameData.links = linkElements
+
+        console.log(`Obtenidos ${linkElements.length} enlaces de tiendas`)
+
+        const fileName = cleanFileName(gameName)
+        fs.writeFile(`./data/${fileName}.json`, JSON.stringify(gameData), (error) => {
+            if (error) {
+                console.error('Error al guardar el archivo:', error)
+            } else {
+                console.log('El texto se ha guardado correctamente en el archivo.')
+            }
+        })
+    } catch {
+        console.error('Error:', error)
+    } finally {
+        await browser.close()
+    }
 }
 
-pt.launch().then(async (browser) => {
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1000, height: 500 });
-  await page.goto(process.argv[2]);
-
-  let gameData = {};
-
-  /*
-    Obtiene los datos del juego desde opencritic
-  */
-  const jsonElement = await page.$('script[type="application/ld+json"]');
-  const jsonString = await page.evaluate(
-    (element) => element.textContent,
-    jsonElement
-  );
-
-  const jsonObject = JSON.parse(jsonString);
-  gameData.data = jsonObject;
-  const gameName = jsonObject.name;
-
-  console.log("Datos del juego extraidos", gameName);
-
-  /*
-    Busca el link en cdkeys a partir del nombre del juego
-  */
-  await page.goto("https://cdkeys.cheap/");
-
-  const inputSelector = ".search-form__field";
-  await page.$(inputSelector);
-  await page.type(inputSelector, gameName.toLowerCase());
-  await timeout(2000);
-  await page.$("#searchList");
-  const firstElementInSearchList = await page.$eval(
-    "#searchList > :first-child a",
-    (element) => element.href
-  );
-
-  console.log(
-    "Enlace del juego buscado obtenido de CDKeys",
-    firstElementInSearchList
-  );
-
-  /*
-    Obtiene los links de las tiendas desde cdkeys
-  */
-  await page.goto(firstElementInSearchList);
-
-  const linkElements = await page.$$eval(".offer__btn", (buttons) => {
-    return buttons.map((button) => {
-      return button.href;
-    });
-  });
-
-  gameData.links = linkElements;
-
-  console.log(`Obtenidos ${linkElements.length} enlaces de tiendas`);
-
-  const fileName = cleanFileName(gameName);
-  fs.writeFile(`./data/${fileName}.json`, JSON.stringify(gameData), (error) => {
-    if (error) {
-      console.error("Error al guardar el archivo:", error);
-    } else {
-      console.log("El texto se ha guardado correctamente en el archivo.");
+;(async () => {
+    for (const [index, game] of gameList.entries()) {
+        await getGameInfo(game, index)
     }
-  });
-
-  await browser.close();
-});
+})()
--- utils/utils.mjs
+++ utils/utils.mjs
@@ -1,9 +1,34 @@
+import fs from 'node:fs'
+
+export const timeout = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
 export const cleanFileName = (fileName) => {
-  // Define a regular expression that allows only alphanumeric characters, underscores, and periods.
-  const validPattern = /[^a-zA-Z0-9_.-]/g;
+    // Define a regular expression that allows only alphanumeric characters, underscores, and periods.
+    const validPattern = /[^a-zA-Z0-9_.-]/g
 
-  // Replace invalid characters with an underscore "_" in this case.
-  const cleanName = fileName.replace(validPattern, "-").toLowerCase();
+    // Replace invalid characters with an underscore "_" in this case.
+    const cleanName = fileName.replace(validPattern, '-').toLowerCase()
 
-  return cleanName;
-};
+    return cleanName
+}
+
+export const readGameListFile = () => {
+    const filePath = 'game-list.txt'
+
+    try {
+        const data = fs.readFileSync(filePath, 'utf-8')
+        const lines = data.split('\r\n')
+
+        while (lines.length > 0 && lines[lines.length - 1] === '') {
+            lines.pop()
+        }
+
+        return lines
+    } catch (error) {
+        console.error('Error reading the game-list file:', error)
+    }
+
+    return []
+}
